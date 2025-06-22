7101ca8086476e4c6f91b78fafb5c45ad3d7b9f0
Gino <gino.casentini.5@gmail.com>
2025-06-22 20:27:47 -0300

    feat(winforms): add persona builder interface

---
+++ Core/Domain/Interfaces/Builders/IPersonaBuilder.cs
@@ -0,0 +1,15 @@
+﻿using SimuladorFacturacion.Core.Domain.Models;
+
+namespace SimuladorFacturacion.Interfaces
+{
+    public interface IPersonaBuilder<T> : IBuilder<T> where T : PersonaModel
+    {
+        IPersonaBuilder<T> SetRazonSocial(string razonSocial);
+        IPersonaBuilder<T> SetCUIT(string cuit);
+        IPersonaBuilder<T> SetDomicilio(string domicilio);
+        IPersonaBuilder<T> SetCondicionIVA(string condicionIVA);
+        IPersonaBuilder<T> SetDatos(string razonSocial, string cuit);
+        IPersonaBuilder<T> SetDatos(string razonSocial, string cuit, string domicilio);
+        IPersonaBuilder<T> SetDatos(string razonSocial, string cuit, string domicilio, string condicionIVA);
+    }
+}
