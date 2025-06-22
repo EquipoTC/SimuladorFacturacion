5f13e32dacb4ac411919ce14153c445fccd5ae76
Gino <gino.casentini.5@gmail.com>
2025-06-22 20:32:26 -0300

    feat(winforms): add renglon builder interface

---
+++ Core/Domain/Interfaces/Builders/IRenglonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using SimuladorFacturacion.Models;
+
+namespace SimuladorFacturacion.Interfaces
+{
+    public interface IRenglonBuilder
+    {
+        IRenglonBuilder SetCodigo(string codigo);
+        IRenglonBuilder SetProductoServicio(string productoServicio);
+        IRenglonBuilder SetCantidad(decimal cantidad);
+        IRenglonBuilder SetUnidadMedida(string unidadMedida);
+        IRenglonBuilder SetPrecioUnitario(decimal precio);
+        IRenglonBuilder SetPorcentajeBonificacion(decimal porcentaje);
+        IRenglonBuilder SetAlicuotaIVA(decimal alicuota);
+        IRenglonBuilder SetDatos(string codigo, string productoServicio, decimal cantidad, string unidadMedida, decimal precioUnitario);
+        IRenglonBuilder SetDatos(string codigo, string productoServicio, decimal cantidad, string unidadMedida, decimal precioUnitario, decimal porcentajeBonificacion);
+        IRenglonBuilder SetDatos(string codigo, string productoServicio, decimal cantidad, string unidadMedida, decimal precioUnitario, decimal porcentajeBonificacion, decimal alicuotaIVA);
+        RenglonModel Build();
+    }
+}
