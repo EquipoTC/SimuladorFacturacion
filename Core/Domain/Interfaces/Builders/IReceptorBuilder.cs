29424e2e737c3811aa9cdde822ba1f3b0c85996f
Gino <gino.casentini.5@gmail.com>
2025-06-22 20:28:32 -0300

    feat(winforms): add receptor builder interface

---
+++ Core/Domain/Interfaces/Builders/IReceptorBuilder.cs
@@ -0,0 +1,10 @@
+﻿using SimuladorFacturacion.Models;
+
+namespace SimuladorFacturacion.Interfaces
+{
+    public interface IReceptorBuilder : IPersonaBuilder<ReceptorModel>
+    {
+        IReceptorBuilder SetEmail(string email);
+        IReceptorBuilder SetDatos(string razonSocial, string cuit, string domicilio, string condicionIVA, string email);
+    }
+}
