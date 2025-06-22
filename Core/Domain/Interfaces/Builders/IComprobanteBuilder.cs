ed02663129bacb45662dade91485500fc48f1feb
Gino <gino.casentini.5@gmail.com>
2025-06-22 20:30:03 -0300

    feat(winforms): add comprobante builder interface

---
+++ Core/Domain/Interfaces/Builders/IComprobanteBuilder.cs
@@ -0,0 +1,17 @@
+﻿using SimuladorFacturacion.Core.Domain.Models;
+using System;
+
+namespace SimuladorFacturacion.Interfaces
+{
+    public interface IComprobanteBuilder : IBuilder<ComprobanteModel>
+    {
+        IComprobanteBuilder SetPuntoVenta(int puntoVenta);
+        IComprobanteBuilder SetNumero(int numero);
+        IComprobanteBuilder SetTipo(string tipo);
+        IComprobanteBuilder SetLetra(string letra);
+        IComprobanteBuilder SetFechaEmision(DateTime fecha);
+        IComprobanteBuilder SetDatos(int puntoVenta, int numero, string tipo);
+        IComprobanteBuilder SetDatos(int puntoVenta, int numero, string tipo, DateTime fecha);
+        IComprobanteBuilder SetDatos(int puntoVenta, int numero, string tipo, string letra, DateTime fecha);
+    }
+}
