ef506f51902887f51e7ba5cc7c700f8bd76498d6
Gino <gino.casentini.5@gmail.com>
2025-06-22 20:45:20 -0300

    feat(winforms): add emisor & receptor builder

---
+++ Infraestructure/Builders/EmisorBuilder.cs
@@ -0,0 +1,21 @@
+﻿using SimuladorFacturacion.Core.Domain.Models;
+using SimuladorFacturacion.Interfaces;
+using System;
+
+namespace SimuladorFacturacion.Builders
+{
+    public class EmisorBuilder : PersonaBuilder<EmisorModel>, IEmisorBuilder
+    {
+        public IEmisorBuilder SetFechaInicioActividades(DateTime fecha)
+        {
+            _persona.FechaInicioActividades = fecha;
+            return this;
+        }
+
+        public IEmisorBuilder SetDatos(string razonSocial, string cuit, string domicilio, string condicionIVA, DateTime fechaInicioActividades)
+        {
+            SetDatos(razonSocial, cuit, domicilio, condicionIVA);
+            return SetFechaInicioActividades(fechaInicioActividades);
+        }
+    }
+}
---
+++ Infraestructure/Builders/ReceptorBuilder.cs
@@ -0,0 +1,20 @@
+﻿using SimuladorFacturacion.Interfaces;
+using SimuladorFacturacion.Models;
+
+namespace SimuladorFacturacion.Builders
+{
+    public class ReceptorBuilder : PersonaBuilder<ReceptorModel>, IReceptorBuilder
+    {
+        public IReceptorBuilder SetEmail(string email)
+        {
+            _persona.Email = email;
+            return this;
+        }
+
+        public IReceptorBuilder SetDatos(string razonSocial, string cuit, string domicilio, string condicionIVA, string email)
+        {
+            SetDatos(razonSocial, cuit, domicilio, condicionIVA);
+            return SetEmail(email);
+        }
+    }
+}
