46368bf23be803cccb01d833bf8f46d964726ee8
Gino <gino.casentini.5@gmail.com>
2025-06-22 20:57:30 -0300

    feat(winforms): add importe builder interface

---
+++ Core/Domain/Interfaces/Builders/IImporteBuilder.cs
@@ -0,0 +1,13 @@
+﻿using SimuladorFacturacion.Core.Domain.Models;
+
+namespace SimuladorFacturacion.Interfaces
+{
+    public interface IImporteBuilder : IBuilder<ImporteModel>
+    {
+        IImporteBuilder SetIVA27(decimal iva27);
+        IImporteBuilder SetIVA21(decimal iva21);
+        IImporteBuilder SetIVA10(decimal iva10);
+        IImporteBuilder SetOtrosTributos(decimal OtrosTributos);
+        IImporteBuilder NetoGravado(decimal NetoGravado);
+    }
+}
