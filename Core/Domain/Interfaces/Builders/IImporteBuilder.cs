97838a3d06b4ccbdddafe2558cebf7b555ae2ed4
Gino <gino.casentini.5@gmail.com>
2025-06-22 21:23:42 -0300

    feat(winforms): add importe builder

--- Core/Domain/Interfaces/Builders/IImporteBuilder.cs
+++ Core/Domain/Interfaces/Builders/IImporteBuilder.cs
@@ -7,7 +7,7 @@ namespace SimuladorFacturacion.Interfaces
         IImporteBuilder SetIVA27(decimal iva27);
         IImporteBuilder SetIVA21(decimal iva21);
         IImporteBuilder SetIVA10(decimal iva10);
-        IImporteBuilder SetOtrosTributos(decimal OtrosTributos);
-        IImporteBuilder NetoGravado(decimal NetoGravado);
+        IImporteBuilder SetOtrosTributos(decimal otrosTributos);
+        IImporteBuilder SetNetoGravado(decimal netoGravado);
     }
 }
---
+++ Infraestructure/Builders/ImporteBuilder.cs
@@ -0,0 +1,59 @@
+﻿using SimuladorFacturacion.Core.Domain.Models;
+using SimuladorFacturacion.Interfaces;
+
+namespace SimuladorFacturacion.Infraestructure.Builders
+{
+    public class ImporteBuilder : IImporteBuilder
+    {
+        private ImporteModel _importes;
+
+        public ImporteBuilder() => Reset();
+
+        public ImporteModel Build()
+        {
+            var result = _importes;
+            Reset();
+            return result;
+        }
+
+        public ImporteModel GetProduct()
+        {
+            return _importes;
+        }
+
+        public void Reset()
+        {
+            _importes = new ImporteModel();
+        }
+
+        public IImporteBuilder SetIVA27(decimal iva27)
+        {
+            _importes.IVA27 = iva27;
+            return this;
+        }
+
+        public IImporteBuilder SetIVA21(decimal iva21)
+        {
+            _importes.IVA21 = iva21;
+            return this;
+        }
+
+        public IImporteBuilder SetIVA10(decimal iva10)
+        {
+            _importes.IVA10 = iva10;
+            return this;
+        }
+
+        public IImporteBuilder SetOtrosTributos(decimal otrosTributos)
+        {
+            _importes.OtrosTributos = otrosTributos;
+            return this;
+        }
+
+        public IImporteBuilder SetNetoGravado(decimal netoGravado)
+        {
+            _importes.NetoGravado = netoGravado;
+            return this;
+        }
+    }
+}
